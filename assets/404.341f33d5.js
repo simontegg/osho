5ab1d5275fb6fdf4f7736009e797efb40b4d7beb
Simon Tegg <admin@macbook-pro.lan>
2022-07-31 14:33:14 +1200

    Updates

---
+++ assets/404.341f33d5.js
@@ -0,0 +1,1 @@
+import{t}from"./index.b0dbaf1e.js";const o=t(`<section class="text-gray-700 p-8"><h1 class="text-2xl font-bold">404: Not Found</h1><p class="mt-4">It's gone \u{1F61E}</p></section>`);function s(){return o.cloneNode(!0)}export{s as default};
